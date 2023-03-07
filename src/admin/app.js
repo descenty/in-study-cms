ad2713410139658bb6b76cd1472e17f2065f7bb2
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-03-07 04:12:59 +0300

    first commit

---
+++ src/admin/app.js
@@ -0,0 +1,16 @@
+export default {
+  config: {
+    locales: ["ru"],
+  },
+  theme: {
+    colors: {
+      primary100: "#f6ecfc",
+      primary200: "#e0c1f4",
+      primary500: "#ac73e6",
+      primary600: "#9736e8",
+      primary700: "#8312d1",
+      danger700: "#b72b1a",
+    },
+  },
+  bootstrap() {},
+};
