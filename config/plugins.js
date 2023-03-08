09f6d7561e25226d5339cfa46fd45d728dfeaf20
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-03-08 19:22:51 +0300

    add openapi

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,30 @@
 module.exports = ({ env }) => ({
- 'transformer': {
+  // "rest-cache": {
+  //   config: {
+  //     provider: {
+  //       name: "memory",
+  //       getTimeout: 500,
+  //       options: {
+  //         max: 32767,
+  //         updateAgeOnGet: false
+  //       },
+  //     },
+  //     strategy: {
+  //       contentTypes: ["api::article.article"],
+  //       debug: true,
+  //     },
+  //   },
+  // },
+  transformer: {
     enabled: true,
     config: {
       responseTransforms: {
         removeAttributesKey: true,
         removeDataKey: true,
       },
-      requestTransforms : {
-        wrapBodyWithDataKey: true
+      requestTransforms: {
+        wrapBodyWithDataKey: true,
       },
-    }
+    },
   },
 });
